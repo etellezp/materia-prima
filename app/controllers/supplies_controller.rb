f0225b7b31261af3e087d88e9d33cd482d800081
github username <github email address>
2018-11-05 23:12:56 -0500

    11212121212 try

--- app/controllers/supplies_controller.rb
+++ app/controllers/supplies_controller.rb
@@ -3,12 +3,13 @@ class SuppliesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @supplies = Supply.all
+
     if params[:supply_name]
       @supplies = Supply.search_supply(params[:supply_name]).order("created_at DESC")
     else
-      @supplies = Supply.all.order("created_at DESC").paginate(page: params[:page], per_page: 10)
+      @supplies = Supply.all
     end
+      @supplies = Supply.all.order("created_at DESC").paginate(page: params[:page], per_page: 10)
   end
 
   def show
