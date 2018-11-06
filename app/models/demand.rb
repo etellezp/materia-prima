8acc669e258db44e0ffc2f5d5131d6ae432804a3
github username <github email address>
2018-11-06 00:38:04 -0500

    Forgot to add PgSearch to demand model

--- app/models/demand.rb
+++ app/models/demand.rb
@@ -1,5 +1,6 @@
 class Demand < ApplicationRecord
+  include PgSearch
   belongs_to :user
 
-  pg_search_scope :search_demand, :against => :name 
+  pg_search_scope :search_demand, :against => :name
 end
