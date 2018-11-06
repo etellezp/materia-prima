7a19a1f80c2f67b9c1fc55c04691f79491948ea5
github username <github email address>
2018-11-05 22:18:33 -0500

    I am getting tired

--- app/controllers/supplies_controller.rb
+++ app/controllers/supplies_controller.rb
@@ -6,9 +6,9 @@ class SuppliesController < ApplicationController
     if params[:supply_name]
       @supplies = Supply.search_supply(params[:supply_name]).order("created_at DESC")
     else
-      @supplies = Supply.all.order("created_at DESC").paginate(page: params[:page], per_page: 10)
+      @supplies = Supply.all
     end
-    @supplies = Supply.all
+    @supplies = Supply.all.order("created_at DESC").paginate(page: params[:page], per_page: 10)
   end
 
   def show
